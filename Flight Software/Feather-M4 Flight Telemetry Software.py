c6f41ecd3b8bb019d7f53e2feaad6aac3c41c083
Brandon <brandonesser435@gmail.com>
2021-12-29 06:56:46 -0600

    v0.1

--- Flight Software/Feather-M4 Flight Telemetry Software.py
+++ Flight Software/Feather-M4 Flight Telemetry Software.py
@@ -9,9 +9,81 @@ def main_notes():   # to collapse the text below in the IDE
 version = "v0.1"
 date = "December 2022"
 
+# idea: add wifi module so it can fetch the current pressure in phoenix. this way we
+# dont have to get it manually then update the code or whateverD
 
 def main():
-    pass
+    import adafruit_bmp3xx
+    import time
+    import analogio
+    import board
+    import digitalio
+    import storage
+    import adafruit
+    import adafruit_sdcard
+
+    vbat_voltage = analogio.AnalogIn(board.D9)
+
+    i2c = board.I2C() # uses board.SCL and board.SDA
+    am2320 = adafruit_am2320.AM2320(i2c)
+
+    SD_CS = board.D10
+    spi = board.SPI()
+    cs = digitalio.DigitalInOut(SD_CS)
+    sd_card = adafruit_sdcard.SDCard(spi, cs)
+    vfs = storage.VfsFat(sd_card)
+    storage.mount(vfs, "/sd_card")
+
+    def get_voltage(pin):
+        return (pin.value * 3.3) / 65536 * 2
+
+
+    print("Logging temperature and humidity to log file")
+
+    initial_time = time.monotonic()
+
+
+
+
+
+
+
+
+
+
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_bmp3xx
+
+# I2C setup
+i2c = board.I2C()  # uses board.SCL and board.SDA
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+
+# SPI setup
+# from digitalio import DigitalInOut, Direction
+# spi = board.SPI()
+# cs = DigitalInOut(board.D5)
+# bmp = adafruit_bmp3xx.BMP3XX_SPI(spi, cs)
+
+
+bmp.altitude
+bmp.sea_level_pressure
+bmp.sea_level_pressure
+bmp.pressure_oversampling = 8
+bmp.temperature_oversampling = 2
+
+while True:
+    print(
+        "Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature)
+    )
+    time.sleep(1)
+
+
+
+
 
 
 
---
+++ Flight Software/get_pressure.py
@@ -0,0 +1,67 @@
+# used to tune the barometer
+
+import requests
+from requests.structures import CaseInsensitiveDict
+import math # sad
+
+
+def weather():
+    global complete_url
+    api_key = ""  # Enter the API key you got from the OpenWeatherMap website
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    zip_code = "53719"
+    use_zip = 0
+    city_name = "scottsdale"
+    
+
+    if use_zip == 0:
+        complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    else:
+        complete_url = base_url + "appid=" + api_key + "&q=" + zip_code
+
+    print("URL sent: " + complete_url)
+
+    response = requests.get(complete_url)
+    x = response.json()
+
+
+    if x["cod"] != "404":
+        if use_zip == 0:
+            print("Check weather! Request cities can return the wrong info")
+            print("Weather in " + city_name)
+        else:
+            print("Weather in " + str(zip_code))
+
+        y = x["main"]
+        current_temperature = y["temp"]
+        z = x["weather"]
+        weather_description = z[0]["description"]
+        current_pressure = y["pressure"]
+        
+
+        print("Temp in Kelvin = " + str(math.trunc(current_temperature)))
+        print("Temp in Celsius = " + str(math.trunc(current_temperature - 273.15)))
+        print("Temp in Fahrenheit = " + str(math.trunc((1.8 * current_temperature) - 459.67)))
+        print("Description = " + str(weather_description))
+        print("Pressure in millibars =" + str(current_pressure))
+        print("Pressure in inches of Hg = " + str(round(current_pressure/33.864, 3)))
+        #print(x) # prints raw json data
+    else:
+        print("City Not Found")
+
+    
+    def response_code():
+        headers = CaseInsensitiveDict()
+        headers["Accept"] = "application/json"
+
+        resp = requests.get(complete_url, headers=headers)
+        print()
+
+        print("Response code: ", end="")
+        print(resp.status_code, end="")
+        if resp.status_code == 200:
+            print(" = Processed request succsessfully")
+    response_code()  
+weather()
+
+
