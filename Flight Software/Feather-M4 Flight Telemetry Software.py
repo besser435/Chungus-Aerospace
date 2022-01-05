a2da7a6843cfe1cc78719cb2a3ba72ece012dbcd
Brandon <brandonesser435@gmail.com>
2022-01-04 17:52:12 -0700

    v0.2.1
    Added possible chute deploy code

--- Flight Software/Feather-M4 Flight Telemetry Software.py
+++ Flight Software/Feather-M4 Flight Telemetry Software.py
@@ -1,13 +1,16 @@
 def main_notes():   # to collapse the text below in the IDE  
     """
-    Flight Telemetry Software for the CircuitPython platform by Joe Mama and besser
+    Flight Software for the CircuitPython platform by Joe Mama and besser
     Made for the Chungus Aerospace Program
+
+    Currenty this only collects data, it doesn't control anything (yet)
     
     https://github.com/besser435?tab=repositories
     """
 
-version = "v0.2"
-date = "December 2022"
+
+version = "v0.2.1"
+date = "January 2022"
 
 # idea: add wifi module so it can fetch the current pressure in phoenix. this way we
 # dont have to get it manually then update the code or whateverD
@@ -59,11 +62,10 @@ def main():
         # file is saved
         time.sleep(1)
 
-    print("Logging to log file")
+        print("Logging to log file")
 
     initial_time = time.monotonic()
-
-
+    #add RTC code
 
 
 
@@ -87,12 +89,27 @@ bmp.sea_level_pressure = 1008
 bmp.pressure_oversampling = 8
 bmp.temperature_oversampling = 2
 
+
+"""
+It should still be enabled to be tested tho
+# proof of concept parachute code
+while True:
+    if bmp.altitude >= 50:  # arms at 50 meters
+        #log the arm event
+        # currently on a while loop so once armed it will just continue to log it. do a check to see if it has been logged already
+            if bmp.altitude <= 49:  # one the rocket sinks below 50 meters it fires the chute
+                #do your mom    # deploys chute
+                #log the deploy event
+                break # get out of this loop somehow so the charge doesnt constantly fire. i dont think this break works
+"""
+
+
+
 while True:
     print(
-        "Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature, bmp.altitude)
-    )
-    print('Altitude: {} meters'.format(bmp.altitude))
-    time.sleep(0.5)
+        "Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.pressure, bmp.temperature,
+        "Altitude: {} meters".format(bmp.altitude)))
+    time.sleep(0.1)
 
 
 
