d16ef3445fd7fec3679347b5bbaef61154c1beb6
Brandon <68145204+besser435@users.noreply.github.com>
2022-01-11 18:40:51 -0700

    v0.1

---
+++ Flight Software/Launch Computer Software.py
@@ -0,0 +1,33 @@
+"""
+NOTE
+---Chungus Aerospace Program---
+https://github.com/besser435/Chungus-Aerospace
+
+
+This is just a concept.
+Its just to get some ideas down for if we even want to build this
+"""
+
+
+version = "v0.1"
+date = "January 2022"
+
+
+"""
+FC = Flight Computer
+
+Get weather data and send that to the FC to calibrate the baro altitude
+Prep FC for launch, like start logging a second before the launch for example
+Control launchpad LEDs
+Send configuration options to the FC
+
+ 
+Fancy menu UI stuff:
+Make a GUI rather than CLI UI, only if the hanging issue is solved. use pygame_menu or something
+
+Checklist
+display telem.
+Connection to the FC status
+Begin launch sequence 
+Fetch data on the FC SD card, delete from FC and store on the host
+"""
