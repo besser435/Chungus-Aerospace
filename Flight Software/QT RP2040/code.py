f6a4e4dba7ca2c9f5896b47562e448ca425b450f
Brandon <68145204+besser435@users.noreply.github.com>
2022-07-04 18:31:34 -0700

    updated lib

--- Flight Software/QT RP2040/code.py
+++ Flight Software/QT RP2040/code.py
@@ -1,7 +1,7 @@
 """
 This is the flight software for the QT Py RP2040
 """
-version = "v1"
+version = "v1.1"
 
 import time
 import board
@@ -12,6 +12,8 @@ import digitalio
 import busio
 from digitalio import DigitalInOut, Direction, Pull
 import gc
+import storage
+
 
 # LED pins and setup
 led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)     
@@ -19,17 +21,35 @@ led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
 # BMP388
 i2c = busio.I2C(board.SCL1, board.SDA1)
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
-bmp.pressure_oversampling = 4
+bmp.pressure_oversampling = 2
 bmp.temperature_oversampling = 2
+
+
 
 # On board button (its the boot select one, in front of the I2C port)
 button = DigitalInOut(board.BUTTON)
 button.direction = Direction.INPUT
 
 
+"""
+How to set up filesystem:
+For launch: Uncomment prep_files(), press button, and you are ready
+
+For REPL viewing and code execution: set dev mode to true, upload that to the drive, 
+then plug into computer with with pin A0 jumped to ground
+
+For editing code/changing files on the drive: just plug in and do whatever
+"""
+
+# Be able to write to storage
+# Needs to be disableed when connect to a computer in order to allow code to execute
+
+
+
+
 # ------------------------- options ------------------------
-launch_delay = 60
-development_mode = 0
+launch_delay = 20
+development_mode = 1
 led_neo.brightness = 1
 file_name = "launch.csv"  
 bmp.sea_level_pressure = 1014       
@@ -38,9 +58,20 @@ event_comma_count = ",,,," # makes sure events go in their own column on the far
 # ------------------------- storage ------------------------
 STARTING_ALTITUDE = bmp.altitude
 data_cycles = 0
-logged_liftoff = 0
 log_list = []
 i = 0
+
+
+def prep_files():
+    print("Waiting for button press to allow writing to storage")
+    led_neo[0] = (255, 255, 0)
+    while True:
+        if button.value == False:
+            break
+    storage.remount("/", False)
+if development_mode == 0:
+    prep_files()
+
 
 while True: # press button to start the countdown
     i = (i + 1) % 256  # run from 0 to 255
@@ -49,7 +80,7 @@ while True: # press button to start the countdown
     if button.value == False:
         break
 
-print("Setting up file and write testing...")
+
 with open(file_name, "a") as f:     # might need to be with open("/" + file_name, "a") as f:
     f.write(",,,,\n")
     f.write("Software version: " + version + "\n")
@@ -58,17 +89,25 @@ with open(file_name, "a") as f:     # might need to be with open("/" + file_name
     f.write("Temperature: {:5.2f},".format(bmp.temperature) + "\n")
     #f.write("Pressure (mbar),Temperature (°c),Altitude (m),Time (s)\n")   
     f.write("Altitude (m),Time (s)\n") 
-print("Done")
 
-
-for i in range(launch_delay):   # countdown
-    led_neo[0] = (255, 255, 255)
-    launch_delay -= 1
-    print("T- " + str(launch_delay))
-    time.sleep(0.5)
-    led_neo[0] = (0, 0, 0)
-    time.sleep(0.5)
-
+# Countdown
+if development_mode == 0:
+    for i in range(launch_delay):   
+        led_neo[0] = (255, 255, 255)
+        launch_delay -= 1
+        print("T- " + str(launch_delay))
+        time.sleep(0.5)
+        led_neo[0] = (0, 0, 0)
+        time.sleep(0.5)
+else:
+    dev_countdown = 3
+    for i in range(3):   
+        led_neo[0] = (255, 0, 255)
+        launch_delay -= 1
+        print("T- " + str(dev_countdown))
+        time.sleep(0.5)
+        led_neo[0] = (0, 0, 0)
+        time.sleep(0.5)
 
 led_neo[0] = (0, 255, 128)
 print("Waiting for liftoff...")
@@ -76,12 +115,7 @@ print("Waiting for liftoff...")
 if development_mode == 0:
     while True:
         if bmp.altitude >= STARTING_ALTITUDE + 2:
-            if logged_liftoff == 0:
-                with open(file_name, "a") as f:
-                    f.write("{:5.2f},".format(bmp.altitude,))
-                    f.write(",Liftoff detected\n")
-                logged_liftoff += 1
-                break
+            break
 
 led_neo[0] = (0, 0, 255) # this is for dev purposes, you wont see it because the rocket will be in the air already
 
@@ -93,22 +127,22 @@ try:
     while True:    
         current_time = time.monotonic()
         time_stamp = current_time - initial_time
-
+        bmp_alt = bmp.altitude
         log_list.extend([
         "\n"
         #"{:5.2f},".format(bmp.pressure),
         #"{:5.2f}".format(bmp.temperature),
-        "{:5.2f}".format(bmp.altitude),
+        "{:5.2f}".format(bmp_alt),
         "{:5.2f}".format(time_stamp)
         ])
         # stops the logging of data
-        if data_cycles > 700:
-            if bmp.altitude <= STARTING_ALTITUDE + 4: 
+        if data_cycles > 300:
+            if bmp_alt <= STARTING_ALTITUDE + 8: 
                 with open(file_name, "a") as f: 
                     f.write("Stopped logging; low altitude met.\n")
                     break
 
-        remainder = data_cycles % 60  
+        remainder = data_cycles % 120
         is_divisible = remainder == 0
         if is_divisible == True:   # this prevents the RAM from running out
             with open(file_name, "a") as f:
@@ -125,15 +159,11 @@ except MemoryError:
     #gc.collect()
     pass # log data incase there is an error that stopped the data logging
 
-print("writing to file system")
+print("writing data to file system...")
 with open(file_name, "a") as f:
     f.write(','.join(log_list))
 print("write done")
 
-
-print("Done")
-while True:     # indicates that the data recording is done
-    i = (i + 1) % 256  # run from 0 to 255
-    led_neo.fill(colorwheel(i)) # Unicorn barf
-    time.sleep(0.01)
+while True:
+    led_neo[0] = (0, 255, 0)
     
