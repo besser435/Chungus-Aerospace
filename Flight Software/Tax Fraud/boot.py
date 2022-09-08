18ea97b0b2a528f17d64b1c0f516c34dadc22db1
Brandon <68145204+besser435@users.noreply.github.com>
2022-09-08 00:07:17 -0700

    Create boot.py

---
+++ Flight Software/Tax Fraud/boot.py
@@ -0,0 +1,29 @@
+import board
+import digitalio
+import storage
+import neopixel
+import time
+led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+switch = digitalio.DigitalInOut(board.D13) # 
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP
+
+
+en_pin = digitalio.DigitalInOut(board.D9)
+en_pin.direction = digitalio.Direction.OUTPUT
+#pin = digitalio.DigitalInOut(board.D13)
+
+# If D13 ia pulled high with a wire CircuitPython can write to the drive
+storage.remount("/", switch.value)
+
+# if the write test is successful, the LED
+try:
+    with open("write_test.csv", "a") as f: 
+        f.write("write success poggers\n")
+    led_neo[0] = (0, 255, 0)
+    time.sleep(1)
+        
+except Exception:
+    led_neo[0] = (255, 0, 0)
+    time.sleep(1)
