d4d772cdd0bd29e33b8c9572386c964cd81b5755
Brandon <68145204+besser435@users.noreply.github.com>
2022-09-17 02:41:26 -0700

    Update check_fraud.java

--- Flight Software/Check Fraud/check_fraud.java
+++ Flight Software/Check Fraud/check_fraud.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Date; 
+
 
 /* Check Fraud Launchpad Software
  *
@@ -6,14 +8,68 @@ import java.util.Scanner;
  */
 
 public class check_fraud {
+   public static boolean debugMode = false;
+   public static int launchCountdown = 10;
+
    public static void main(String[] args) {
-      Scanner scnr = new Scanner(System.in);
       mainMenu();
-      scnr.close();
+   }
+
+   
+   static void relay(int state) {
+      if (debugMode == false){   //this is so im not deafened while working on the thing
+         if (state == 0) {
+            //relay off;
+         }
+         if (state == 1) {
+            //relay on;
+         }
+      }
    }
 
 
+   static void beeper(int state) {
+      if (debugMode == false){   //this is so im not deafened while working on the thing
+         if (state == 0) {
+            //beeper off;
+         }
+         if (state == 1) {
+            //beeper on;
+         }
+      }
+   }
+   
 
+   static void ignition() {
+      System.out.println("Relay On");
+      //Thread.sleep(1000);
+
+   }
+
+
+   static void delayCountdown() {
+      Scanner scnr = new Scanner(System.in);
+      System.out.println("Are you sure you want to start the countdown? y/n ");
+
+      
+      String whichOption = scnr.nextLine();
+      //scnr.close();
+      if (whichOption.equals("y")) {
+         System.out.println("Starting Countdown. Press CTRL + C to cancel");
+         //Thread.sleep(1000);
+         for (int i = launchCountdown; i > 0; i--) {
+            System.out.println(i);
+         }
+         
+
+
+
+      } else {
+         System.out.println("Countdown Cancelled");
+         mainMenu();
+      }
+      scnr.close(); // FUCK YOU ORACLE
+   }
 
 
    static void mainMenu() {
@@ -22,19 +78,21 @@ public class check_fraud {
       System.out.println("1. Begin Ignition Countdown");
       System.out.println("2. Ignite Now");
       System.out.print("Enter an option: ");
-  
 
       int whichOption = scnr.nextInt();  
-      if (whichOption == 1) {
-         System.out.println("sent 1"); 
+      //scnr.close();
+      switch (whichOption) {
+         case 1:
+            delayCountdown();
+            break;
+         case 2:
+            //ignite();
+            break;
+         default:
+            System.out.println("Invalid option");
+            mainMenu();
+            break;
       }
-      else if (whichOption == 2) {
-         System.out.println("sent 2");  
-      }
-      else {
-         System.out.println("Invalid Option");  
-      }
-
-      scnr.close();
+      scnr.close();  // FUCK YOU
    }
 }
