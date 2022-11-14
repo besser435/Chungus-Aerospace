ac5d51d54c08aaa9fa46be06107e6495317a46c3
Brandon <68145204+besser435@users.noreply.github.com>
2022-11-14 00:09:48 -0700

    v1.1
    Made the code a bit neater

--- Flight Software/QT RP2040/Velocity data/code.py
+++ Flight Software/QT RP2040/Velocity data/code.py
@@ -1,45 +1,42 @@
-import board
-import adafruit_icm20x
-import time
-import busio
-import math
-import time
-# I2C Bus
+import board, time, busio, math, adafruit_icm20x
 i2c = busio.I2C(board.SCL1, board.SDA1)
-
-# ICM
 icm = adafruit_icm20x.ICM20649(i2c)
 adafruit_icm20x.ICM20649.accelerometer_range = 4
 adafruit_icm20x.accelerometer_data_rate = 5000
+
+version = 1.1
+
 # Variables for determining acceleration/velocity
 g_divisor = 1
 cum_g = 0
 
 time.sleep(5)
-#Accelerometer calibration (which axis is up)
-# up_axis = max(abs(icm.acceleration[0]), abs(icm.acceleration[1]), abs(icm.acceleration[2]))
-#Accelerometer g calibration
-timer = 0
-while True:
+# Accelerometer calibration (which axis is up)
+#up_axis = max(abs(icm.acceleration[0]), abs(icm.acceleration[1]), abs(icm.acceleration[2]))
+
+# Accelerometer g calibration
+for i in range(2000):
     icm_accel = icm.acceleration
     accel_x = icm_accel[0]
     accel_y = icm_accel[1]
     accel_z = icm_accel[2]
+
     accel_mag = math.sqrt(pow(accel_x, 2) + pow(accel_y, 2) + pow(accel_z, 2))
     cum_g += accel_mag
     g = cum_g / g_divisor
     g_divisor += 1
+
     print(str(g))
     #if accel_mag > 30:
-    timer += 1
-    if timer > 2000:
-        break
+        #pass
+
 adafruit_icm20x.ICM20649.accelerometer_range = 30
 initial_time = time.monotonic()
 vel_0 = 0
 time_0 = 0
 accel_0 = 0
-#Ascent mode
+
+# Ascent mode
 while True: #accel_mag > 30 or v0 > 10:
     icm_accel = icm.acceleration
     current_time = time.monotonic()
@@ -49,6 +46,7 @@ while True: #accel_mag > 30 or v0 > 10:
     accel_z = icm_accel[2]
     accel_mag = math.sqrt(pow(accel_x, 2) + pow(accel_y, 2) + pow(accel_z, 2))
     # USE WITH Z AXIS FACING (UP) OR ACCELEROMETER WILL GET CONFUSED
+
     if accel_z < 0:
         accel_sign = -1
     else:
@@ -57,5 +55,10 @@ while True: #accel_mag > 30 or v0 > 10:
     time_0 += time_stamp
     vel_0 += vel_1
     accel_0 += (accel_mag * accel_sign)
+
     print("accel data: " + str(icm_accel) + " " + str(accel_mag) + " " + str(vel_1) + " " + str(time_stamp))
-    #with open
+    
+    
+"""with open("data.csv", "a") as f:
+    f.write()
+    print("wrote data")"""
