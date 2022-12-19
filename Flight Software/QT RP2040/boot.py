71cf24aae126d83d5fd912ad1e80346f5cc275a4
Brandon <68145204+besser435@users.noreply.github.com>
2022-12-19 04:51:38 -0700

    Update boot.py

--- Flight Software/QT RP2040/boot.py
+++ Flight Software/QT RP2040/boot.py
@@ -1,8 +1,13 @@
-"""modified version of the Tax Fraud boot file"""
+"""
+modified version of the Tax Fraud boot file
+This needs to be modified to work with the board that it is going to run on.
+Those modifications are what pins are used
+"""
 
 import board
 import digitalio
 import neopixel
+import storage
 import time
 
 led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -12,6 +17,9 @@ switch.pull = digitalio.Pull.UP
 
 # NOTE to be able to write to the drive for uploading and deleting files, the wire must be pulled to 5V
 # if the write test is successful, the LED lights up green
+
+storage.remount("/", not switch.value) # Pull switch to ground to enable onboard writes to the drive
+
 try:
     with open("write_test.txt", "a") as f: 
         f.write("write success poggers\n")
@@ -20,4 +28,4 @@ try:
 
 except Exception:
     led_neo[0] = (255, 0, 0)
-    time.sleep(1)
+    time.sleep(2)
