751e8b2b49fd68e0b68e55df3822cfa16e580ac5
Brandon <68145204+besser435@users.noreply.github.com>
2022-12-25 00:35:35 -0600

    Update boot.py

--- Flight Software/CALC/boot.py
+++ Flight Software/CALC/boot.py
@@ -9,6 +9,11 @@ import digitalio
 import neopixel
 import storage
 import time
+import adafruit_pcf8523
+
+i2c = board.I2C() 
+rtc = adafruit_pcf8523.PCF8523(i2c)
+t = rtc.datetime
 
 led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
 switch = digitalio.DigitalInOut(board.A0) 
@@ -19,10 +24,10 @@ switch.pull = digitalio.Pull.UP
 # if the write test is successful, the LED lights up green
 
 storage.remount("/", not switch.value) # Pull switch to ground to enable onboard writes to the drive
-
+#led_neo.brightness = 0.02
 try:
     with open("write_test.txt", "a") as f: 
-        f.write("write success poggers\n")
+        f.write("write test success at " + "%d-%d-%d" % (t.tm_mon, t.tm_mday, t.tm_year) + " %d:%02d:%02d" % (t.tm_hour, t.tm_min, t.tm_sec) + "\n")
     led_neo[0] = (0, 255, 0)
     time.sleep(1)
 
