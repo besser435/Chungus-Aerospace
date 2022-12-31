5121271091adfc35a9091d58f273ed9b0f010c0c
Brandon <68145204+besser435@users.noreply.github.com>
2022-12-31 10:31:36 -0600

    v1.15

--- Flight Software/CALC/CALC.py
+++ Flight Software/CALC/CALC.py
@@ -8,7 +8,7 @@ pinouts are configured for.
 
 #NOTE This code doesnt work currently. The log list will also fill up the memory, causing an unhandled exception.
 """
-version = "v1.15-beta.1"
+version = "v1.15"
 date = "December 2022"
 
 #import adafruit_adxl34x
@@ -52,7 +52,7 @@ bmp.temperature_oversampling = 2
 
 # Accelerometer go throughout the code and uncomment the NOTE lines to enable the accelerometer
 #accel = adafruit_adxl34x.ADXL345(i2c)
-#accel = adafruit_icm20x.ICM20649(i2c)
+#NOTE accel = adafruit_icm20x.ICM20649(i2c)
 
 # Real time clock
 rtc = adafruit_pcf8523.PCF8523(i2c)
@@ -71,12 +71,12 @@ beeper.direction = digitalio.Direction.OUTPUT
 development_mode = 0        # edits things like countdown so the code can be tested easier without having to change several other options
 #CHUTE_DEPLOY_ALT = 50  #NOTE not used. This would be less prone to errors, maybe use it
 #CHUTE_ARM_OFFSET = 5       # alt separation in meters between when the chute is armed and when it is deployed
-mute_beeper = 1
+mute_beeper = 0
 bmp.sea_level_pressure = 1013
 log_stop_count = 400        # when to stop logging after an amount of data points are collected, backup to low altitude condition
 launch_countdown = 10       # Waits this many seconds before the logging starts. Delay so the rocket can be set up to launch before logging starts
+chute_trigger_time = 2      # seconds the chute relay will be triggered for
 FILE_NAME = "launch " + "%d-%d-%d" % (t.tm_mon, t.tm_mday, t.tm_year) + " %d;%02d;%02d" % (t.tm_hour, t.tm_min, t.tm_sec) + ".csv"  # pure stupidity
-event_comma_count = ",,,"   # makes sure events go in their own column on the far right 
 
 
 # ------------------------- storage ------------------------
@@ -85,6 +85,10 @@ STARTING_ALTITUDE = bmp.altitude
 logged_liftoff = 0
 data_cycles = 1
 log_list = []
+chute_armed = 0
+chute_deployed = 0
+logged_chute_deploy = 0 
+chute_deploy_timestamp = None   # is this needed here, or can the vars access each other in the loop?
 
 
 def beep(state):
@@ -110,21 +114,19 @@ def write(data):
         
 def write_test():   
     """
-    will throw an error amd stop the countdown if something is bunged. boot.py should catch this, but this is here for reasons.
+    Will throw an error amd stop the countdown if something is bunged. boot.py should catch this, but this is here for reasons.
     
     Normally during .csv creation, the drive is written to, and if that failed it would throw an error. However
     I catch those errors incase they happen during the main loop, that way the main loop can continue.
 
     This will throw an uncaught error if the drive is bunged, so I know there is an issue before putting the computer in the rocket.
-    
     """
 
     with open("write_test.txt", "a") as f: 
-        f.write("write test success in CALC.py at " + "%d-%d-%d" % (t.tm_mon, t.tm_mday, t.tm_year) + " %d:%02d:%02d" % (t.tm_hour, t.tm_min, t.tm_sec) + "\n")
+        f.write("write test success in CALC.py at " + "%d-%d-%d" % (t.tm_mon, t.tm_mday, t.tm_year) + " %d:%02d:%02d" % (t.tm_hour, t.tm_min, t.tm_sec) + "\n\n")
 write_test()
 
-# .csv creation and formatting
-def create_csv():
+def create_csv():   # .csv creation and formatting
     #with open("/sd/" + FILE_NAME, "a") as f: 
     write(",,,,,\n")  # creates the right amount of columns 
     write("Date: %d/%d/%d" % (t.tm_mon, t.tm_mday, t.tm_year) + ",,,,,\n")   # The extra commas is is GitHub doesnt get cranky
@@ -165,11 +167,11 @@ else:  # shows that the code is running
     for i in range(launch_countdown):
         launch_countdown -= 1
         print("T- " + str(launch_countdown))
-        led_neo.fill((255, 0, 0))
+        led_neo[0] = (255, 0, 0)
         beep(1)
         time.sleep(0.5)
 
-        led_neo.fill((0, 0, 0))
+        led_neo[0] = (0, 0, 0)
         beep(0)
         time.sleep(0.5)
     
@@ -179,8 +181,8 @@ if not development_mode:
     print("Waiting for liftoff")
     led_neo[0] = (255, 69, 0)   # indicates waiting for liftoff 
     while True:
-#NOTE DEV MODE NUMBER HERE
-        if bmp.altitude >= STARTING_ALTITUDE + 0:
+#NOTE
+        if bmp.altitude >= STARTING_ALTITUDE + 5:
             if not logged_liftoff:
                 write("{:5.2f},".format(bmp.altitude,))
                 logged_liftoff += 1
@@ -189,15 +191,6 @@ if not development_mode:
 #                          ------------------------ Main data logging code ------------------------
 led.value = True  # turn on LED to indicate writing has started
 initial_time = time.monotonic() # needs to be here and not in the time set ups bits code for reasons
-
-
-chute_armed = 0
-chute_deployed = 0
-logged_chute_deploy = 0 
-
-chute_deploy_timestamp = None   # is this needed here, or can the vars access each other in the loop?
-chute_trigger_time = 2  # seconds the chute relay will be triggered for
-
 def main_logging():
     global chute_armed, chute_deployed, logged_chute_deploy, data_cycles, landed, chute_deploy_timestamp, chute_trigger_time
     led_neo[0] = (0, 0, 255)
@@ -205,7 +198,7 @@ def main_logging():
 
     while True:   
         bmp_alt = bmp.altitude
-        #NOTE accel_xyz = accel.acceleration
+#NOTE accel_xyz = accel.acceleration
     
         current_time = time.monotonic()
         time_stamp = current_time - initial_time
@@ -218,19 +211,19 @@ def main_logging():
         "{:5.2f}".format(time_stamp)
         ])
 
-#NOTE DEV MODE ALTS SET, NOT REAL VALUES
+#NOTE 
         # Arm parachute
         #if bmp_alt >= STARTING_ALTITUDE + (CHUTE_DEPLOY_ALT - CHUTE_ARM_OFFSET):  # NOTE copilot code. check old code to see that it matches this to make sure it works
         if not chute_armed: # prevents the event from being ran repeatedly
-            if bmp_alt >= STARTING_ALTITUDE + 0:  # arm altitude. should be a few meters above the deploy altitude so ensure the rocket is descending
+            if bmp_alt >= STARTING_ALTITUDE + 50:  # arm altitude. should be a few meters above the deploy altitude so ensure the rocket is descending
                 log_list.extend(["Armed parachute,"])
                 print("Armed parachute")
                 chute_armed += 1
-#NOTE DEV MODE ALTS SET, NOT REAL VALUES
+#NOTE 
         # Deploy parachute
         if chute_armed: 
             if not chute_deployed: # prevents the event from being ran repeatedly 
-                if bmp_alt <= STARTING_ALTITUDE + 0:  # deploy altitude
+                if bmp_alt <= STARTING_ALTITUDE + 45:  # deploy altitude
                     chute_deploy_timestamp = time_stamp
                     chute_relay.value = True
                     log_list.extend(["Deployed parachute,"])
@@ -251,9 +244,9 @@ def main_logging():
 #NOTE
         # stops the logging of data
         if data_cycles >= log_stop_count:  # ensures that the logging is not stopped on the pad  
-            if bmp_alt <= STARTING_ALTITUDE + 15:   # + 15 is incase it lands above the starting elevation or the sensor drifts
+            if bmp_alt <= STARTING_ALTITUDE + 15:   # +15 is incase it lands above the starting elevation or the sensor drifts
                 log_list.extend(["Stopped logging low alt met,"])
-                print("Stopped logging low alt met {:5.2f}".format(bmp.altitude) + "m")
+                print("Stopped logging, low alt met {:5.2f}".format(bmp.altitude) + "m")
                 print("Starting altitude: " + str(STARTING_ALTITUDE) + "m")
                 break
             
@@ -268,7 +261,7 @@ def main_logging():
         is_divisible = remainder == 0
         if is_divisible:   
             #with open(FILE_NAME , "a") as f:
-            print("Writing to SD")
+            print("Storing log")
             write(','.join(log_list))
             log_list.clear()
             log_list.extend([",RAM flushed \n"])
@@ -276,7 +269,7 @@ def main_logging():
 
         data_cycles += 1 
         print("Data cycles: " + str(data_cycles) + "   Time: %.2f" % time_stamp + "   Free RAM: " + str(gc.mem_free()))
-        #raise Exception("handle test")
+        #raise Exception("error handle test")
 
 """#NOTE this doesnt work. When it runs out of memory, it cant log the list and throws another error.
 while not landed: # why are there two identical loops? I have no idea, but they are both needed! Thank you programming.
