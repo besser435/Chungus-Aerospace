0058403287fa9c35a27e731b4b8b141bdaa3219a
Brandon <68145204+besser435@users.noreply.github.com>
2022-12-31 01:23:06 -0600

    Update code.py

--- Flight Software/QT RP2040/Velocity data/Pre-calibration code/Accelerometer inaccuracy and g calibration/code.py
+++ Flight Software/QT RP2040/Velocity data/Pre-calibration code/Accelerometer inaccuracy and g calibration/code.py
@@ -11,16 +11,16 @@ led0 = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 #x/y calibration setup and initial timer
 led0[0] = (0, 255, 0)
+print("X/Y delay")
 time.sleep(20)
-
 cum_x = 0
 cum_y = 0
 i = 1
 
 #x/y calibration
+led0[0] = (255, 0, 0)
+print("Running X/Y calibration")
 for i in range(2000):
-    led0[0] = (255, 0, 0)
-    
     cum_x += icm.acceleration[0]
     cum_y += icm.acceleration[1]
     avg_x = cum_x / i
@@ -28,20 +28,22 @@ for i in range(2000):
     print("x: " + str(avg_x) + "y: " + str(avg_y) + "z: " + "g: ")
 
 #z calibration setup and timer
+led0[0] = (0, 255, 0)
+print("Z/g delay")
+time.sleep(20)
 cum_z = 0
 i = 1
-led0[0] = (0, 255, 0)
-time.sleep(20)
 
 #z calibration
+led0[0] = (255, 0, 0)
+print("Running Z calibration")
 for i in range(2000):
-    led0[0] = (255, 0, 0)
     cum_z += icm.acceleration[2]
     avg_z = cum_z / i
     print("x: " + str(avg_x) + "y: " + str(avg_y) + "z: " + str(avg_z) + "g: ")
 
 #g calibration
-led0[0] = (0, 255, 255)
+print("Running Z calibration")
 i = 1
 cum_g = 0
 for i in range(2000):
@@ -51,6 +53,7 @@ for i in range(2000):
     cum_g += accel_mag
     g = cum_g / i
     print("x: " + str(avg_x) + "y: " + str(avg_y) + "z: " + str(avg_z) + "g: " + str(g))
+
 print("Calibration complete")
-led0[0] = (0, )
+led0[0] = (255, 255, 255)
 #Printing the values and manually putting them in the code is janky as fuck, but idk how to improve it
