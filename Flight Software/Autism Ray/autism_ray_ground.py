cc14cae2525f9b687a582ba8d4e2d27cb3693574
Brandon <68145204+besser435@users.noreply.github.com>
2023-03-09 03:07:59 -0700

    v0.2

--- Flight Software/Autism Ray/autism_ray_ground.py
+++ Flight Software/Autism Ray/autism_ray_ground.py
@@ -1,10 +1,11 @@
 from digitalio import DigitalInOut
-import time, busio, digitalio, board, neopixel, adafruit_rfm9x, storage, adafruit_ssd1306
-import analogio, traceback, pulseio, datetime, traceback
+import time, busio, digitalio, board, adafruit_rfm9x, adafruit_ssd1306
+import traceback, datetime, traceback
 from colorama import init
 init()
 from colorama import Fore
 init(autoreset=True)
+import RPi.GPIO as GPIO
 
 """
 Authored by besser435, March 2023
@@ -19,21 +20,17 @@ but we might as well try.
 
 This is the ground station code, it will help us find the rocket.
 """
-
-version = "Autism Ray v0.1 (Ground)"
+version = "Autism Ray v0.2 (Ground)"
 
 # I2C
 i2c = busio.I2C(board.SCL, board.SDA)
-
-# Neopixel
-led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 # RFM95
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.D18)
 reset = digitalio.DigitalInOut(board.D19)
-rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
-rfm9x.tx_power = 23
+rfm = adafruit_rfm9x.RFM9x(spi, cs, reset, 900.0) # NOTE 900MHz
+rfm.tx_power = 23
 prev_packet = None
 
 # 128x32 OLED Display
@@ -45,58 +42,59 @@ width = display.width
 height = display.height
 
 # Buzzer
-buzzer = pulseio.PWMOut(board.D5, variable_frequency=True)
-buzzer.frequency = 100
-OFF = 0
-ON = 2**15
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)  # PWM pin 4 on the Pi
+buzz = GPIO.PWM(4, 1000) 
+GPIO.setwarnings(False)
 
 # date and file path
 now = datetime.datetime.now()
-time_and_date = now.strftime("%m-%d-%Y  %H:%M")
+time_and_date = now.strftime("%m-%d-%Y %H:%M")
 FILE_LOCATION = "/home/pi/Desktop/" 
 
 
 try:
-    #raise UnicodeDecodeError   # for testing
     while True:
         packet = None   # this is to reset the vars for the next loop iteration
         rssi = None
-        packet = rfm9x.receive()
-        rssi = rfm9x.last_rssi
-        
-        print("\n" * 20)
+        packet = rfm.receive()
+        rssi = rfm.last_rssi
+
         if packet is None:
             print(Fore.YELLOW + "Received nothing! Listening again...")
+            buzz.start(0)
+            display.fill(0)
+            display.text("Received nothing!", 0, 0, 1)
+            display.show()
         else:
-            packet_text = str(packet, 'utf-8')  # UTF-8 untested. on TF it was sent as UFT-8, and decoded as ASCII
+            packet_text = str(packet, 'utf-8') 
             print("Received (UTF-8): {0}".format(packet_text))
             print(Fore.CYAN + "RSSI:", rssi, "dB", "   at", time_and_date)
-            
-            buzzer.duty_cycle = ON
+            buzz.start(50)
+            buzz.ChangeFrequency((rssi * 4 + 900))
 
-            # +900 makes the pitch higher as the signal get stronger. *4 shifts it to more comfy frequencies
-            print("DEBUG RSSI buzzer freq: ", rssi * 4 + 900)
-            buzzer.frequency = rssi * 4 + 900
-
+            # +900 makes the pitch higher as the signal gets stronger. *4 shifts it to more comfy frequencies
+            print("DEBUG RSSI buzzer freq: ", rssi * 5 + 900)
+           
             display.fill(0)
-            display.text(packet_text, 0, 0, 1)
-            display.text(rssi, 0, 13, 1)
+            display.text(packet_text[11:], 0, 0, 1)
+            display.text("RSSI: " + str(rssi), 0, 13, 1)
             display.text(time_and_date, 0, 25, 1)
             display.show()
-
-        time.sleep(0.1)
+        time.sleep(0.4)
 
 except Exception as e:
     print("Error occurred: \n")
     print(Fore.LIGHTRED_EX + str(traceback.format_exc()))
-    buzzer.duty_cycle = OFF
-    
+    buzz.start(0)
+    display.fill(0)
+    display.show()
     with open(FILE_LOCATION + "beacon_receiver_error.txt", "a") as f: 
         f.write(str(time_and_date) + "\n" + str(traceback.format_exc()))
         f.write("\n" * 2)
 
 except KeyboardInterrupt:
     print("KeyboardInterrupt")
-    buzzer.duty_cycle = OFF
+    buzz.start(0)
     display.fill(0)
     display.show()
--- Flight Software/Autism Ray/code.py
+++ Flight Software/Autism Ray/code.py
@@ -1,6 +1,5 @@
 from digitalio import DigitalInOut
-import time, busio, digitalio, board, neopixel, adafruit_rfm9x, storage
-import analogio, traceback
+import time, busio, digitalio, board, neopixel, adafruit_rfm9x
 
 """
 Authored by besser435, March 2023
@@ -16,30 +15,25 @@ but we might as well try.
 This is the rocket code. It will broadcast the radio signal, and
 will also beep for when we get close to finding it.
 """
-version = "Autism Ray v0.1 (Rocket)"
-
-# I2C
-i2c = busio.I2C(board.SCL, board.SDA)
+version = "Autism Ray v0.2 (Rocket)"
 
 # Neopixel
-led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
-
-
-
 """TODO Pin numbers are not set correctly."""
+led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=1)
 
 
 # RFM95
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-cs = digitalio.DigitalInOut(board.D18)
-reset = digitalio.DigitalInOut(board.D19)
-rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
-rfm9x.tx_power = 23
-prev_packet = None
+"""spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.D7)
+reset = digitalio.DigitalInOut(board.D0)
+rfm = adafruit_rfm9x.RFM9x(spi, cs, reset, 900.0) # NOTE 900MHz
+rfm.tx_power = 23
+prev_packet = None"""
 
 # Beeper
 mute_beeper = 0
-beeper = digitalio.DigitalInOut(board.D5) 
+beeper = digitalio.DigitalInOut(board.D1) 
+beeper.direction = digitalio.Direction.OUTPUT
 def beep(state):
     if mute_beeper == 0:
         if state == 0:
@@ -49,48 +43,40 @@ def beep(state):
 
 
 def error(e):
-    print("Error occurred: \n")
-    print(e)
+    print("Error occurred: \n", e)
     
-    storage.remount("/", True)  # untested, might need to be set to False. just test it in general
-    with open("error.txt", "a") as f: 
-        f.write(e, "\n")
-
-    for i in range(120):
+    for i in range(120):    # wait 2 minutes for the issue to magically disappear before trying one last time
         led_neo.fill((255, 0, 0))
-        beeper(1)
+        beep(1)
         time.sleep(0.5)
         led_neo.fill((0, 0, 0))
-        beeper(0)
+        beep(0)
         time.sleep(0.5)
 
+        print("Error, will try again when i is 120. i = " + str(i))
+
     while True:
-        send_data = bytes("Error occurred: \n" + str(e) + "\r\n","utf-8")
-        rfm9x.send(send_data)
-        print("Sent the message: " + send_data)
+        send_data = bytes("Error occurred: \n" + e + "\r\n","utf-8")
+        #rfm.send(send_data)
+        print("Sent the message: " + str(send_data))
         time.sleep(1)
 
 
 try:
     heartbeat = 0
     while True:
-        time.sleep(0.5)
+        time.sleep(0.2)
         led_neo.fill((255, 255, 255))
-        beeper(1)
+        beep(1)
 
-        send_data = bytes("AR Beacon. Heartbeat: " + str(heartbeat) + "\r\n","utf-8")
-        rfm9x.send(send_data)
-        print("Sent the message: " + send_data)
+        send_data = bytes(version + " Beacon. Heartbeat: " + str(heartbeat) + "\r\n","utf-8")
+        #rfm.send(send_data)
+        print("Sent the message: " + str(send_data))
 
-        time.sleep(0.5)
+        time.sleep(0.2)
         led_neo.fill((0, 0, 0))
-        beeper(0)
+        beep(0)
         heartbeat += 1
-        
-
-except OSError as e:
-    error(e)
+            
 except Exception as e:
     error(e)
-
-    
