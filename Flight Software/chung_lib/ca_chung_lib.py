1711475e8ea2abee6f6473ea81fd416df35062f1
Brandon <68145204+besser435@users.noreply.github.com>
2023-06-15 02:49:24 -0700

    Create ca_chung_lib.py

---
+++ Flight Software/chung_lib/ca_chung_lib.py
@@ -0,0 +1,48 @@
+import adafruit_icm20x, adafruit_bmp3xx, csv, random, logging
+
+
+#NOTE https://realpython.com/python-logging/ do this properly
+
+
+class rocket_telemetry:
+    def __init__(self, starting_alt=None):  
+        self.starting_alt = None #NOTE not done
+        self.spoof_current_alt = None
+        self.spoof_current_accel = None
+        pass
+
+    # https://www.geeksforgeeks.org/getter-and-setter-in-python/
+
+
+    @property
+    def current_alt(self):
+        if not self.spoof_current_alt:
+            return 30 #adafruit_bmp3xx.altitude
+
+    @property
+    def current_accel(self):
+        """Returns a tuple of the current acceleration in m/s^2, assuming values aren't spoofed."""
+        if not self.spoof_current_accel:
+            return (0, 0, 9.8) #adafruit_icm20x.acceleration
+        #NOTE add spoofed setting. see acceleration function below
+        
+        # in the future this will read values from a file rather that
+        # generating random values. This will be used to test flight code.
+        if self.spoof_current_accel:    
+            logging.warning("Spoofing current accel values. Not reading from sensor.")
+            rand_x = random.randint(-20, 20)
+            rand_y = random.randint(-20, 20)
+            rand_z = random.randint(-20, 20)
+            return (rand_x, rand_y, rand_z)
+            
+
+    @property
+    def taken_off(self):
+        if self.current_alt > self.starting_alt + 5:
+            self.has_taken_off = True  # stores the state in the object once set
+        return self.has_taken_off
+
+    @property
+    def test_func(self):
+        return "test"
+    
