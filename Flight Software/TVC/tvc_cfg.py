be5e257f0ffc9970acec70992f7144e77a25f5fb
AutisticAndAshamed <98008253+AutisticAndAshamed@users.noreply.github.com>
2023-07-01 02:39:12 -0700

    TVC configuration
    something something conflicted file idk seems fine to me

---
+++ Flight Software/TVC/tvc_cfg.py
@@ -0,0 +1,16 @@
+#TVC config file
+
+#IMU
+accel_x_offset = None
+accel_y_offset = None
+accel_z_offset = None
+
+gyro_x_offset = None
+gyro_y_offset = None
+gyro_z_offset = None
+
+#Geofencing for abort condition
+limit_north = None
+limit_south = None
+limit_east = None
+limit_west = None
