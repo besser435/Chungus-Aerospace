ed9cb737bb9478af1dffe6086debad4ea7adac95
Brandon <brandonesser435@gmail.com>
2024-07-23 11:43:17 -0700

    Update ChungRF tests

--- Flight Software/ChungRF/tx_test/tx_test.ino
+++ Flight Software/ChungRF/tx_test/tx_test.ino
@@ -1,45 +1,18 @@
-/*
-  RadioLib SX126x Blocking Transmit Example
-
-  This example transmits packets using SX1262 LoRa radio module.
-  Each packet contains up to 256 bytes of data, in the form of:
-  - Arduino String
-  - null-terminated char array (C-string)
-  - arbitrary binary data (byte array)
-
-  Other modules from SX126x family can also be used.
-
-  Using blocking transmit is not recommended, as it will lead
-  to inefficient use of processor time!
-  Instead, interrupt transmit is recommended.
-
-  For default module settings, see the wiki page
-  https://github.com/jgromes/RadioLib/wiki/Default-configuration#sx126x---lora-modem
-
-  For full API reference, see the GitHub Pages
-  https://jgromes.github.io/RadioLib/
-*/
-
-// include the library
 #include <RadioLib.h>
 
-// SX1262 has the following connections:
-// NSS pin:   10
-// DIO1 pin:  2
-// NRST pin:  3
-// BUSY pin:  9
+// ESP32-S2 Feather:
+// NSS pin:   4, SCL
+// DIO1 pin:  9, D9
+// NRST pin:  11, D11
+// BUSY pin:  13, D13
 SX1262 radio = new Module(4, 9, 11, 13);
 
-// or using RadioShield
-// https://github.com/jgromes/RadioShield
-//SX1262 radio = RadioShield.ModuleA;
-
-// or using CubeCell
-//SX1262 radio = new Module(RADIOLIB_BUILTIN_MODULE);
-
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
 
   // initialize SX1262 with default settings
   Serial.print(F("[SX1262] Initializing ... "));
@@ -61,26 +34,20 @@ void setup() {
   /*
     radio.setRfSwitchPins(4, 5);
   */
-
 }
 
 // counter to keep track of transmitted packets
 int count = 0;
 
 void loop() {
-  Serial.print(F("[SX1262] Transmitting packet ... "));
+  Serial.println(F("[SX1262] Transmitting packet ... "));
 
   // you can transmit C-string or Arduino string up to
   // 256 characters long
-  digitalWrite(LED_BUILTIN, HIGH);
-  String str = "Hello World! #" + String(count++);
+  String str = "ChungRF Test. Packet " + String(count++);
   int state = radio.transmit(str);
+  Serial.println(str);
 
-  // you can also transmit byte array up to 256 bytes long
-  /*
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x56, 0x78, 0xAB, 0xCD, 0xEF};
-    int state = radio.transmit(byteArr, 8);
-  */
 
   if (state == RADIOLIB_ERR_NONE) {
     // the packet was successfully transmitted
@@ -105,8 +72,6 @@ void loop() {
     Serial.println(state);
 
   }
-  delay(200);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 
   // wait for a second before transmitting again
   delay(1000);
